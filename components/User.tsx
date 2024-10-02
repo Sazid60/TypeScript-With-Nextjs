2b4721f9a306e64b59b47692d00bf58ecc7f9407
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-02 10:36:32 +0600

    Prop Type Using interface

--- components/User.tsx
+++ components/User.tsx
@@ -12,13 +12,31 @@
 
 // export default User;
 
-type UserShape = {
+// type UserShape = {
+//     name: string,
+//      age: number,
+//       isStudent :boolean
+// }
+
+// const User = ({name, age, isStudent}:UserShape) => {
+//     return (
+//         <div>
+//             <h1>{name}</h1>
+//             <h1>{age}</h1>
+//             <h1>{isStudent}</h1>
+//         </div>
+//     );
+// };
+
+// export default User;
+
+interface UserShape {
     name: string,
-     age: number,
-      isStudent :boolean
+    age: number,
+    isStudent: boolean
 }
 
-const User = ({name, age, isStudent}:UserShape) => {
+const User = ({ name, age, isStudent }: UserShape) => {
     return (
         <div>
             <h1>{name}</h1>
